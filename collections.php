7fa0f7dd51434daaccfe6de83c70e920ace66304
M. Drew LaMar <mdlama@wm.edu>
2024-02-17 11:09:25 -0500

    [feat] Can now override group collections view
    (cherry picked from commit c56acbfdac3b4d4a299b01164b788e1c8fe1b059)

--- collections.php
+++ collections.php
@@ -8,10 +8,12 @@
 // No direct access
 defined('_HZEXEC_') or die();
 
+require_once PATH_APP . DS . 'libraries' . DS . 'Qubeshub' . DS . 'Plugin' . DS . 'Plugin.php';
+
 /**
  * Groups Plugin class for assets
  */
-class plgGroupsCollections extends \Hubzero\Plugin\Plugin
+class plgGroupsCollections extends \Qubeshub\Plugin\Plugin
 {
 	/**
 	 * Affects constructor behavior. If true, language files will be loaded automatically.
